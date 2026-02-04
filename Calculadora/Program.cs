1648932a6a3dfb4e6f552317355bd54af949dce1
Victor Hugo <victorhugo.mota26@gmail.com>
2026-02-04 10:35:19 -0300

    first commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Escolha qual Operação Usar:");
+Console.WriteLine("1 - Adição");
+Console.WriteLine("2 - Substituição");
+Console.WriteLine("3 - Divisão");
+Console.WriteLine("4 - Multiplicação");
+
+var Decicao = Console.ReadLine();
