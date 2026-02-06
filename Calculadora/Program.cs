f5b9476a5bd67d1ff194832f1f9cd2ce6fc7e472
Victor Hugo <victorhugo.mota26@gmail.com>
2026-02-05 21:05:11 -0300

    Refactor Program

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,8 +1,56 @@
-﻿Console.WriteLine("Escolha qual Operação Usar:");
+﻿inicio:
+ Console.WriteLine("Escolha qual Operação Usar:");
 Console.WriteLine("1 - Adição");
 Console.WriteLine("2 - Substituição");
 Console.WriteLine("3 - Divisão");
 Console.WriteLine("4 - Multiplicação");
 
-var Deciçao = Console.ReadLine();
+var Decisao = Console.ReadLine();
+int Decisao = 0;
+
+try
+{
+    Decisao INT = Convert.ToInt16(Decisao);
+}
+catch
+{
+    Console.WhiteLine("Valor Invalido!");
+    goto inicio;
+}
+
+// 1° Numero --
+
+Console.WhiteLine("Digite o 1° Numero: ");
+var Num1 = ConsoleColor.ReadLine();
+
+float NumeroConvertido;
+
+try
+{
+    var NumeroConvertido = float.Parse(Num1).Replace(",",",",".");
+}
+catch
+{
+    Console.Clear();
+    goto inicio;
+}
+
+// 2° Numero --
+
+Console.WhiteLine("Digite o 2° Numero: ");
+var Num2 = ConsoleColor.ReadLine();
+
+float NumeroConvertido;
+
+try
+{
+    var NumeroConvertido = float.Parse(Num1).Replace(",",",",".");
+}
+catch
+{
+    Console.Clear();
+    goto inicio;
+}
+
+//
 
